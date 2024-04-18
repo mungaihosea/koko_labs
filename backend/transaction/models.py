9392d79688918e09698f191ed017538a5c696509
mungai <mungaihosea@gmail.com>
2024-04-18 08:03:51 +0300

    Added model filters

--- backend/transaction/models.py
+++ backend/transaction/models.py
@@ -3,7 +3,7 @@ from uuid import uuid4
 
 
 class BaseModel(models.Model):
-    id = models.CharField(max_length = 255, default = uuid4, primary_key=True)
+    id = models.CharField(max_length = 255, default = uuid4, primary_key=True, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
--- backend/transaction/views.py
+++ backend/transaction/views.py
@@ -6,14 +6,12 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 
 
-
-
 class TransactionViewSet(viewsets.ModelViewSet):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
     pagination_class = StandardPaginator
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
-    filterset_fields = ['user'] 
-    search_fields = ['description']  # Add fields for searching
+    filterset_fields = ['user', 'type']     # Fields to filter by 
+    search_fields = ['description']         # Database fields to search by
 
 
